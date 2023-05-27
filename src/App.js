8127ad2cd79f8113c2439150ca5df738ee1e4b2e
Rohan Dave <rohandewangdave@gmail.com>
2023-05-28 03:16:54 +0530

    confirm booking page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import Booking from './components/pages/Bookings/Booking';
 import Layout from './components/layout/Layout';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/booking' element={<Booking />} />
+            <Route path='/booking-confirmed' element={<ConfirmedBooking />} />
           </Routes>
         </Layout>
 
--- src/components/pages/Bookings/Booking.js
+++ src/components/pages/Bookings/Booking.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BookingForm from './BookingForm';
 import { fetchAPI, submitAPI } from '../../../utils/api';
 
@@ -13,11 +14,18 @@ function updateTimes(availableTimes, date) {
 
 function Booking() {
     const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+    const navigate = useNavigate();
     const occasions = ["birthday", "anniversary"];
+
+    const handleSubmit = (formData) => {
+        const response = submitAPI(formData);
+        navigate('/booking-confirmed');
+        return response;
+    }
 
     return (
         <BookingForm
-            handleSubmit={submitAPI}
+            handleSubmit={handleSubmit}
             occasions={occasions}
             availableTimes={availableTimes}
             dispatchDateChange={dispatch}
